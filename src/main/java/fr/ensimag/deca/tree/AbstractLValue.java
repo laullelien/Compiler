a5e6b394ee2dcf216a829cc32d9d97620c5b55b5
Sarah Ramaharobandro <sarah.ramaharobandro@grenoble-inp.org>
2024-01-11 14:03:56 +0100

    AstractLValue completion began

--- src/main/java/fr/ensimag/deca/tree/AbstractLValue.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractLValue.java
@@ -13,4 +13,21 @@ import fr.ensimag.deca.context.EnvironmentExp;
  * @date 01/01/2024
  */
 public abstract class AbstractLValue extends AbstractExpr {
+    // Peut être override par les classes qui étendent AbstractLValue (règle 3.64)
+    // public abstract Type verifyLValue(DecacCompiler compiler, EnvironmentExp localEnv,
+    //                           ClassDefinition currentClass) throws ContextualError;
+
+    // pas sûr qu'il faut faire ça
+    private AbstractIdentifier varName;
+
+    public Type verifyLValueIdent(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass)
+            throws ContextualError {
+        // règle 3.69
+        Type resType = varName.verifyExpr(compiler, localEnv, currentClass);
+        return resType;
+    }
+
+    public void codeGenIdent(DecacCompiler compiler) {
+        varName.codeGenInst(compiler);
+    }
 }
