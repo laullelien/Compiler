ca09746e095f66ad93a2a901f195055844a1e1b9
turkiea <antoine.turkie@grenoble-inp.org>
2024-01-12 14:58:54 +0100

    Division par 0 établie pour int et float.

--- src/main/java/fr/ensimag/deca/tree/Divide.java
+++ src/main/java/fr/ensimag/deca/tree/Divide.java
@@ -2,6 +2,8 @@ package fr.ensimag.deca.tree;
 
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.ima.pseudocode.ImmediateFloat;
+import fr.ensimag.ima.pseudocode.ImmediateInteger;
 import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.instructions.*;
 
@@ -20,10 +22,21 @@ public class Divide extends AbstractOpArith {
     @Override
     protected void codeGenInst(DecacCompiler compiler) {
         super.codeGenInst(compiler);
-        if (getType().isInt())
-            compiler.addInstruction(new QUO(Register.getR(3), Register.getR(2))); // division entière
+        if (getType().isInt()) {
+            if (getRightOperand().getDval().equals(new ImmediateInteger(0))){
+                throw new IllegalArgumentException("La division est par 0, attention.");
+            }
+            else {
+                compiler.addInstruction(new QUO(Register.getR(3), Register.getR(2))); // division entière
+            }
+        }
         else // nous avons une division de flottant
-            compiler.addInstruction(new DIV(Register.getR(3), Register.getR(2)));
+            if (getRightOperand().getDval().equals(new ImmediateFloat(0))) {
+                throw new IllegalArgumentException("La division est par 0, attention.");
+            }
+            else {
+                compiler.addInstruction(new DIV(Register.getR(3), Register.getR(2)));
+            }
 
     }
 
