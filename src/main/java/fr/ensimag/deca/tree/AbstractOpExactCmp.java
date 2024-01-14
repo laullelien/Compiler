de2b718c4da94eee709b7ddaf336f98ac6c64d49
Aurélien Delory <aurelien.delory@grenoble-inp.org>
2024-01-14 11:35:19 +0100

    ajout if

--- src/main/java/fr/ensimag/deca/tree/AbstractOpExactCmp.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpExactCmp.java
@@ -1,6 +1,5 @@
 package fr.ensimag.deca.tree;
 
-
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
@@ -8,7 +7,6 @@ import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
 
 /**
- *
  * @author gl38
  * @date 01/01/2024
  */
@@ -24,17 +22,17 @@ public abstract class AbstractOpExactCmp extends AbstractOpCmp {
         Type leftOperandType = this.getLeftOperand().verifyExpr(compiler, localEnv, currentClass);
         Type rightOperandType = this.getRightOperand().verifyExpr(compiler, localEnv, currentClass);
         //les opérandes sont tous les 2 des nombres
-        if((leftOperandType.isFloat() || leftOperandType.isInt()) && (rightOperandType.isFloat() || rightOperandType.isInt())) {
-            if(leftOperandType.isInt()) {
-                setLeftOperand(new ConvFloat(getLeftOperand()));
-                getLeftOperand().setType(compiler.environmentType.FLOAT);
+        if ((leftOperandType.isFloat() || leftOperandType.isInt()) && (rightOperandType.isFloat() || rightOperandType.isInt())) {
+            if (leftOperandType != rightOperandType) {
+                if (leftOperandType.isInt()) {
+                    setLeftOperand(new ConvFloat(getLeftOperand()));
+                    getLeftOperand().setType(compiler.environmentType.FLOAT);
+                } else {
+                    setRightOperand(new ConvFloat(getRightOperand()));
+                    getRightOperand().setType(compiler.environmentType.FLOAT);
+                }
             }
-            else {
-                setRightOperand(new ConvFloat(getRightOperand()));
-                getRightOperand().setType(compiler.environmentType.FLOAT);
-            }
-        }
-        else if(!(leftOperandType.isBoolean() && rightOperandType.isBoolean())) {
+        } else if (!(leftOperandType.isBoolean() && rightOperandType.isBoolean())) {
             //Plus tard ajouter la compatibilité avec les objets
             // type_binary_op
             throw new ContextualError("Les types des opérandes de la comparaison ne sont pas compatibles. Regle 3.33", this.getLocation());
