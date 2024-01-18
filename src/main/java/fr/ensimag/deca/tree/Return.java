b3aa02f06efc5b1fdb12e1d8e2535e1f31ce828c
Augustin Chenevois <augustin.chenevois@grenoble-inp.org>
2024-01-18 19:37:44 +0100

    étape B ok pour Return

--- src/main/java/fr/ensimag/deca/tree/Return.java
+++ src/main/java/fr/ensimag/deca/tree/Return.java
@@ -5,19 +5,46 @@ import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
 
-public class Return extends AbstractUnaryExpr {
-    public Return(AbstractExpr operand){
-        super(operand);
+import java.io.PrintStream;
+
+public class Return extends AbstractInst {
+
+    private AbstractExpr returnExpr;
+    public Return(AbstractExpr returnExpr){
+        this.returnExpr = returnExpr;
+    }
+
+    public AbstractExpr getReturnExpr(){
+        return this.returnExpr;
     }
 
     @Override
-    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
-        return null;
+    protected void verifyInst(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass, Type expectedReturnType) throws ContextualError {
+        if (expectedReturnType.isVoid()){
+            throw new ContextualError("Le type retourné par la fonction est nulle : la règle (3.24) n'est pas respectée.", this.getLocation());
+        }
+        this.getReturnExpr().verifyRValue(compiler, localEnv, currentClass, expectedReturnType);
     }
 
     @Override
-    protected String getOperatorName() {
-        return null;
+    protected void codeGenInst(DecacCompiler compiler) {
+
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+        this.returnExpr.prettyPrint(s, prefix, true);
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+        this.returnExpr.iter(f);
     }
 }
