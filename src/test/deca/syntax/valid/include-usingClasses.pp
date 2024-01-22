50349c88b3d199bc2ced1706f2795adcc395bf75
Augustin Chenevois <augustin.chenevois@grenoble-inp.org>
2024-01-22 11:49:08 +0100

    test include pour lex, synt et context

---
+++ src/test/deca/syntax/valid/include-usingClasses.pp
@@ -0,0 +1,28 @@
+class A extends Object {
+	int method () {
+		return 3;
+	}
+
+}
+
+class B extends A {
+	protected int d = 3;
+	float jaiFaim () {
+		return 0x1.8p1;
+	}
+
+}
+
+class C extends B {
+	boolean isTrue () {
+		print("I don't care about the name");
+		return true;
+	}
+
+}
+
+{
+	A a = new A();
+	print(a.method);
+	print(caNeSertARienInstancierDansLeTestSynth);
+}
